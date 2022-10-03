3500672f1555c0e0e31da3b31571898cea0109e7
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-04 00:09:37 +0300

    homework-6 updated

--- home-work-6/index.js
+++ home-work-6/index.js
@@ -1,9 +1,32 @@
 const Koa = require("koa");
-const app = new Koa();
+const Router = require("koa-router");
 
-// response
-app.use((ctx) => {
-  ctx.body = "Hello Koa";
+const app = new Koa();
+const router = new Router();
+const port = 3000;
+
+router.get("/", (ctx) => {
+  ctx.status = 200;
+  ctx.body = "<h1>Hakan KARAYILMAZ</h1>";
 });
 
-app.listen(5000);
+router.get("/index", (ctx) => {
+  ctx.status = 200;
+  ctx.body = "<h1>Index sayfasına hoşgeldiniz</h1>";
+});
+
+router.get("/hakkimda", (ctx) => {
+  ctx.status = 200;
+  ctx.body = "<h1>Hakkımda sayfasına hoşgeldiniz</h1>";
+});
+
+router.get("/iletisim", (ctx) => {
+  ctx.status = 200;
+  ctx.body = "<h1>İletişim sayfasına hoşgeldiniz</h1>";
+});
+
+app.use(router.routes());
+
+app.listen(port, () => {
+  console.log(`${port} portunda sunucu başlatıldı.`);
+});
