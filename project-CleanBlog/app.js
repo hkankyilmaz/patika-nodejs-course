8bf4d6ef8b031e23bcd07b711470d38316cbbdfa
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-04 18:23:41 +0300

    static file uploaded, .ejs files uploaded

--- project-CleanBlog/app.js
+++ project-CleanBlog/app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const app = express();
-const port = 2000;
+const ejs = require("ejs");
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" };
+//view engine
+app.set("view engine", "ejs");
 
+//static files
+app.use(express.static("public"));
+
+//routing
 app.get("/", (req, res) => {
-  app.send(blog);
+  res.render("index");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
 });
 
+const port = 2000;
 app.listen(port, () => {
   console.log(`App has been started on ${port} port...`);
 });
